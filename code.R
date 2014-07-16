93d6e122c8e9aa07981f27f75c6f9a048f5775a0
timelyportfolio <kent.russell@timelyportfolio.com>
2014-07-16 11:17:49 -0500

    add github badge for replication

--- code.R
+++ code.R
@@ -59,7 +59,15 @@ tags %$%
      , script(src = "js/color-scheme.min.js")
    , "</head>" %>% HTML
    , body(
-      div( class = "container"
+      a(
+        href = "https://github.com/timelyportfolio/rCharts_colors"
+        ,img(
+          style="position: absolute; top: 0; right: 0; border: 0;"
+          , src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+          , alt="Fork me on GitHub"
+        )
+      )
+      ,div( class = "container"
         ,div( class="page-header"
            , h1 ("rCharts + dimple Color Picker"
              , small(
