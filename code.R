9b9d89970dd07fd8f6710d6f3ed0d4bc3668eda4
timelyportfolio <kent.russell@timelyportfolio.com>
2014-07-16 12:10:07 -0500

    save code with new palette title

--- code.R
+++ code.R
@@ -324,8 +324,7 @@ tags %$%
                   .style("width",d3.format(".4%")(1/colors.length))
                   .style("height",d3.select("#picker").style("height"))
                   .style("background",function(d){return d;})
-                  .append("title")
-                    .text(function(d){return d})
+                  .attr("title",function(d){return d})
 
                 updateChartColors(colors);
               }
